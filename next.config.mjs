3fbd769dc5339849c259c0f9c0c0de1493793c38
Ashutosh Dangi <ashutoshdangi1010@gmail.com>
2024-07-04 17:01:11 +0530

    Deploy to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withOptimizedImages from 'next-export-optimize-images';
+
+const nextConfig = withOptimizedImages({
+  basePath: '/your-repository-name',
+  assetPrefix: '/your-repository-name/',
+  output: 'export'
+});
 
 export default nextConfig;
